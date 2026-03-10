656d337d863d4de019f4f067ab823b732385414e
sourav <souravnegi144@gmail.com>
2026-03-10 23:04:59 +0530

    first route added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js"; // ✅ import app from app.js
 
-dotenv.config({
-  path: "./env",
-});
+dotenv.config({ path: "./.env" });
 
-let PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 connectDB()
   .then(() => {
-    app.listen(PORT || 8000, () => {
-      console.log(`Server is running at port : ${PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Server is running at port: ${PORT}`);
     });
   })
   .catch((err) => {
-    console.log(`MONGO db connection failed !!!`, err);
+    console.log(`MongoDB connection failed!`, err);
   });
