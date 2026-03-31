d4992b67ec64673ec2a1a20282722bebf3f89c48
sourav <souravnegi144@gmail.com>
2026-03-31 22:19:44 +0530

    Login functionality added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,6 +6,28 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 // to check user is exist or not we have to import user from mongoose
 import { User } from "../models/user.model.js";
 
+// easy to create a seperate method to generate userToken or accessToken
+
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    // add these into user
+    user.refreshToken = refreshToken;
+    // save
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "something went wrong while generating refresh and access token"
+    );
+  }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
   // validation = not empty
@@ -104,4 +126,78 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, createdUser, "User registered successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  // req.body -> data
+  // username or email exist
+  // find the user
+  // password check
+  // access and refresh token
+  // send these token via cookies
+  // respose - successfully login
+
+  //1. take data from req.body
+
+  const { email, username, password } = req.body;
+
+  // need for both
+  if (!username || !email) {
+    throw new ApiError(400, "username or email is required");
+  }
+
+  // now find in db
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  // if user not found
+
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
+
+  // if user found now check password
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credential");
+  }
+
+  // refresh token and access token
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  // optional
+  const loggedInUser = User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  // send it to the cookies
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        { user: loggedInUser, accessToken, refreshToken },
+        "User logged in successfully"
+      )
+    );
+});
+
+const logOutUser = asyncHandler(async (req, res) => {
+  // but the problem here is i want user id but how
+  // so in this case we have middleware - jare ho to milke jana
+  // for this we can design our own middleware like multer.js we have for fotos
+  // so we can create a auth middleware
+})
+
+export { registerUser, loginUser };
